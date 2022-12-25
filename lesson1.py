65bb604b255f09b64067215a7c8a24b745e6de32
Владислав <koldaev18@mail.ru>
2022-12-25 17:32:29 +0300

    create file lesson1

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,7 @@
-'dfgdggddg'
+a=float(input('Vvedite storonu a'))
+b=float(input('Vvedite storonu b'))
+c=float(input('Vvedite storonu c'))
+if a+b>c and a+c>b and c+b>a:
+    print('существует')
+else:
+    print('HE существует')

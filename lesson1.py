9f97e9c720402293e28fa02bf1987b08384c8f8b
Владислав <koldaev18@mail.ru>
2022-12-26 15:55:22 +0300

    create file lesson1

--- lesson1.py
+++ lesson1.py
@@ -1,8 +1,23 @@
-a=input('Vvedite vozrast (1;99) ')
-b=a[-1]
-if int(a)>11 and b=='1':
-    print('мне ',a,' год')
-elif int(b)>=2 and int(b)<=4:
-    print('мне ', a, ' годa')
+a1=input('Введите день месяц год первого \n')
+a2=input()
+a3=input()
+print(a1, a2, a3)
+b1=input('Введите день месяц год второго \n')
+b2=input()
+b3=input()
+print(b1, b2, b3)
+if a3<b3:
+    print('Первый старше')
+elif a3==b3:
+    if a2<b2:
+        print('Первый старше')
+    elif a2==b2:
+        if a1<b1:
+            print('Первый старше')
+        elif a1==b1:
+            print('Они ровесники')
+        else:('Второй старше')
+    else:
+        print('Второй старше')
 else:
-    print('мне ', a, ' лет')
+    print('Второй старше')
